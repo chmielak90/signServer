5a67fefad1c5de1a27be8cdecfe7bade91efebf7
Michal Chmielewski <michal.chmielewski@optitex.com>
2023-05-17 11:18:58 +0200

    server.py - added TSL connection with handling multiple connection

--- server.py
+++ server.py
@@ -1,43 +1,63 @@
-import logging
 import socket
+import ssl
+import threading
+import logging
+import os
 
-HOST = ''  # Listen on all available interfaces
-PORT = 9090
-BUFFER_SIZE = 1024 * 1024 * 1024
+HOST = ''  # Listen on all IP addresses
+PORT = 9090  # Server port
+BUFFER_SIZE = 1024 * 1024 * 1024  # 1024MB
+ALLOWED_CLIENTS_DIR = 'allowed_clients'  # Directory containing allowed clients' certificates
 
 # Configure logging
 logging.basicConfig(filename='server.log', level=logging.INFO, format='%(asctime)s %(levelname)-8s %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S')
 
-def handle_client(client_socket):
-    # Receive data from the client
-    data = client_socket.recv(BUFFER_SIZE).decode('utf-8')
-    print(f"Received data from client: {data}")
+def handle_client(conn, client_addr, context):
+    logging.info(f'Got connection from {client_addr}')
+    connstream = context.wrap_socket(conn, server_side=True)
 
-    # Send a response back to the client
-    response = "Hello, client! I received your message."
-    client_socket.sendall(response.encode('utf-8'))
+    # Certificate pinning
+    client_cert = connstream.getpeercert(binary_form=True)
+    allowed = False
+    for certfile in os.listdir(ALLOWED_CLIENTS_DIR):
+        with open(os.path.join(ALLOWED_CLIENTS_DIR, certfile), 'rb') as f:
+            allowed_cert_data = f.read()
+        try:
+            # Try to decode as PEM
+            allowed_cert_pem = allowed_cert_data.decode('utf-8')
+            allowed_cert_der = ssl.PEM_cert_to_DER_cert(allowed_cert_pem)
+        except (UnicodeDecodeError, ValueError):
+            # Not a PEM file, assume it's DER
+            allowed_cert_der = allowed_cert_data
+        if client_cert == allowed_cert_der:
+            allowed = True
+            break
+    if not allowed:
+        logging.info(f'Refused connection from {client_addr}: certificate not recognized')
+        return
 
-    # Close the client socket
-    client_socket.close()
+    connstream.shutdown(socket.SHUT_RDWR)
+    connstream.close()
 
-# Create a socket object
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Main function to start the server
+def main():
+    # Create a context for TLS
+    context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    context.verify_mode = ssl.CERT_REQUIRED
+    context.load_cert_chain(certfile="server.crt", keyfile="server.key")  # Load server certificate and private key
+    context.load_verify_locations(cafile="chain.pem")  #
 
-# Allow reuse of the address
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # Load chain of trusted client certificates
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((HOST, PORT))
+    server_socket.listen()
 
-# Bind the socket to a specific address and port
-server_socket.bind((HOST, PORT))
+    while True:
+        client_conn, client_addr = server_socket.accept()
+        threading.Thread(target=handle_client, args=(client_conn, client_addr, context)).start()
 
-# Listen for incoming connections
-server_socket.listen(1)
-print(f"Server listening on {HOST}:{PORT}")
 
-while True:
-    # Accept a client connection
-    client_socket, client_address = server_socket.accept()
-    print(f"Connected to client: {client_address[0]}:{client_address[1]}")
+if __name__ == '__main__':
+    main()
 
-    # Handle the client connection in a separate thread
-    handle_client(client_socket)
