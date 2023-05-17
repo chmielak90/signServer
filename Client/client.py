fd6e29f07c37a69f2c5dbffc0b182ffa1f70d4d7
Michal Chmielewski <michal.chmielewski@optitex.com>
2023-05-17 13:09:27 +0200

    client.py - create ssl context

--- Client/client.py
+++ Client/client.py
@@ -1,4 +1,5 @@
 import configparser
+import ssl
 
 # Read configuration from INI file
 config = configparser.ConfigParser()
@@ -7,3 +8,8 @@ config.read('config.ini')
 HOST = config.get('Server', 'Host')  # Server IP
 PORT = config.getint('Server', 'Port')  # Server port
 BUFFER_SIZE = config.getint('Server', 'Buffer_Size')  # Buffer size
+
+# Create an SSL context
+context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+context.load_cert_chain(certfile=config.get('Client', 'Certfile'), keyfile=config.get('Client', 'Keyfile'))
+context.load_verify_locations(cafile=config.get('Client', 'Chainfile'))
