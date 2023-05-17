44dab12091c43c877572104888966345604f08f5
Michal Chmielewski <michal.chmielewski@optitex.com>
2023-05-17 13:08:16 +0200

    client.py - starting configuration with using external config.ini file

---
+++ Client/client.py
@@ -0,0 +1,9 @@
+import configparser
+
+# Read configuration from INI file
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+HOST = config.get('Server', 'Host')  # Server IP
+PORT = config.getint('Server', 'Port')  # Server port
+BUFFER_SIZE = config.getint('Server', 'Buffer_Size')  # Buffer size
